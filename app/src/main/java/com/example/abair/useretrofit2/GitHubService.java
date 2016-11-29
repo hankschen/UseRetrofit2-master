37ff9bf3fe42100ea56ee2133a2e1c4a9e716e59
HanksChen <cmnofadsl@yahoo.com.tw>
2016-11-29 21:37:36 +0800

    teacher Retrofit for database

---
+++ app/src/main/java/com/example/abair/useretrofit2/GitHubService.java
@@ -0,0 +1,50 @@
+package com.example.abair.useretrofit2;
+
+import java.util.List;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+/**
+ * Created by abair on 2016/11/18.
+ */
+public interface GitHubService {
+//    @GET("users/{user}/repos")
+//    Call<List<Repo>> listRepos(@Path("user") String user);
+
+    @GET("api/readData.php")
+    Call<List<Repo>> listRepos();
+
+    @GET("api/api_delete.php")
+    Call<ResponseBody> delete(@Query("cID") String cID);
+
+//    @POST("api/api_add_post.php")
+//    Call<ResponseBody> add(@Body Repo repo);
+//
+//    @POST("api/api_add_post.php")
+//    Call<ResponseBody> addByPlainText(@Body RequestBody body);
+
+    @FormUrlEncoded
+    @POST("api/api_add_post.php")
+    Call<ResponseBody> addByFormPost(@Field("cName") String cName,
+                                     @Field("cSex") String cSex,
+                                     @Field("cBirthday") String cBirthday,
+                                     @Field("cEmail") String cEmail,
+                                     @Field("cPhone") String cPhone,
+                                     @Field("cAddr") String cAddr);
+
+    @GET("api/api_update_get.php")
+    Call<ResponseBody> updateByGet( @Query("cID") int cID,
+                                    @Query("cName") String cName,
+                                    @Query("cSex") String cSex,
+                                    @Query("cBirthday") String cBirthday,
+                                    @Query("cEmail") String cEmail,
+                                    @Query("cPhone") String cPhone,
+                                    @Query("cAddr") String cAddr);
+}
